2809de301bec5f12b8d7c837246af7c9d6f8c3ac
adam-weiler <adam.weiler@yahoo.ca>
2019-07-31 16:10:00 -0400

    Adding tables for Breed, Clinic, Owner, Vets, Pets, and Appointments.

--- oneToMany/oneToMany/admin.py
+++ oneToMany/oneToMany/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from oneToMany.models import Play, Actor, Role, Director
+from oneToMany.models import Play, Actor, Director, Role
+from oneToMany.models import Breed, Clinic, Owner, Veterinarian, Pet, Appointment
 
-admin.site.register(Play)
+admin.site.register(Play) #Play must be called first.
 admin.site.register(Actor)
-admin.site.register(Role)
 admin.site.register(Director)
+admin.site.register(Role)
+
+admin.site.register(Breed) #Breed must be written first.
+admin.site.register(Clinic) #Clinic must be written first.
+admin.site.register(Owner) #Owner must be written first.
+admin.site.register(Veterinarian) #Veterinarian must be written second.
+admin.site.register(Pet) #Pet must be written second.
+admin.site.register(Appointment)
--- oneToMany/oneToMany/models.py
+++ oneToMany/oneToMany/models.py
@@ -2,9 +2,10 @@ from django.conf import settings #Needed for foreignkey.
 from django.contrib.auth.models import User
 from django.db import models
 # from django.forms import ModelForm
-# from django.utils import timezone # Needed for timezone.localtime()
+from django.utils import timezone # Needed for timezone.localtime()
 
-class Play(models.Model):
+#Question 1 - Plays:
+class Play(models.Model): #Play must be written first.
     play_name = models.CharField(max_length=255)
 
     def __str__(self):
@@ -17,6 +18,13 @@ class Actor(models.Model):
     def __str__(self):
         return f"actor_name = {self.actor_name}"
 
+class Director(models.Model):
+    director_name = models.CharField(max_length=255)
+    play_id = models.ForeignKey(Play, on_delete=models.CASCADE, related_name='directors')
+
+    def __str__(self):
+        return f"director_name = {self.director_name}"
+
 class Role(models.Model):
     character_name = models.CharField(max_length=255)
     actor_id = models.ForeignKey(Actor, on_delete=models.CASCADE, related_name='roles')
@@ -24,9 +32,50 @@ class Role(models.Model):
     def __str__(self):
         return f"character name = {self.character_name}"
 
-class Director(models.Model):
-    director_name = models.CharField(max_length=255)
-    play_id = models.ForeignKey(Play, on_delete=models.CASCADE, related_name='directors')
+
+
+# Question 2 - Vet clinic:
+
+class Breed(models.Model): #Breed must be written first.
+    breed_name = models.CharField(max_length=255)
 
     def __str__(self):
-        return f"director_name = {self.director_name}"
+        return f"breed_name = {self.breed_name}"
+
+class Clinic(models.Model): #Clinic must be written first.
+    clinic_name = models.CharField(max_length=255)
+    clinic_address = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"clinic_name = {self.clinic_name}"
+
+class Owner(models.Model): #Owner must be written first.
+    owner_name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"owner_name = {self.owner_name}"
+
+class Veterinarian(models.Model): #Veterinarian must be written second.
+    veterinarian_name = models.CharField(max_length=255)
+    clinic_id = models.ForeignKey(Clinic, on_delete=models.CASCADE, related_name='clinics')
+
+    def __str__(self):
+        return f"veterinarian_name = {self.veterinarian_name}"
+
+class Pet(models.Model): #Pet must be written second.
+    pet_name = models.CharField(max_length=255)
+    owner_id = models.ForeignKey(Owner, on_delete=models.CASCADE, related_name='owners')
+    breed_id = models.ForeignKey(Breed, on_delete=models.CASCADE, related_name='breeds')
+
+    def __str__(self):
+        return f"pet_name = {self.pet_name}"
+
+class Appointment(models.Model):
+    appointment_date = models.DateTimeField(default=timezone.localtime())
+    veterinarian_id = models.ForeignKey(Veterinarian, on_delete=models.CASCADE, related_name='veterinarians')
+    pet_id = models.ForeignKey(Pet, on_delete=models.CASCADE, related_name='pets')
+
+    def __str__(self):
+        return f"appointment_date = {self.appointment_date}"
+
