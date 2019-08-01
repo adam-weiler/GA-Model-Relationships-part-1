9a9a77cb04d9fdb5351abe10d631ade65d905311
adam-weiler <adam.weiler@yahoo.ca>
2019-08-01 18:19:29 -0400

    Adding Author, Book, Reader, Authorship, Books_Read, Chapter tables.

--- manyToMany/manyToMany/admin.py
+++ manyToMany/manyToMany/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from manyToMany.models import Film, Viewer, Films_Watched
+from manyToMany.models import Author, Book, Reader, Authorship, Books_Read, Chapter
 
 admin.site.register(Film) #Play must be called first.
-admin.site.register(Viewer)
-admin.site.register(Films_Watched)
+admin.site.register(Viewer) #Play must be called first.
+admin.site.register(Films_Watched) #Has Many Through
+
+admin.site.register(Author) #Author must be called first.
+admin.site.register(Book) #Book must be called first.
+admin.site.register(Reader) #Book must be called first.
+admin.site.register(Authorship) #Has Many Through
+admin.site.register(Books_Read) #Has Many Through
+admin.site.register(Chapter)
--- manyToMany/manyToMany/models.py
+++ manyToMany/manyToMany/models.py
@@ -6,8 +6,7 @@ from django.utils import timezone # Needed for timezone.localtime()
 
 
 # 1. Imagine an app that allows users to keep track of what films they've seen:
-
-class Film(models.Model):
+class Film(models.Model): #Film must be written first.
     title = models.CharField(max_length=255)
     director = models.CharField(max_length=255)
     year = models.IntegerField()
@@ -15,7 +14,7 @@ class Film(models.Model):
     def __str__(self):
         return f'title = {self.title}'
 
-class Viewer(models.Model):
+class Viewer(models.Model): #Viewer must be written first.
     user_name = models.CharField(max_length=255)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
@@ -24,15 +23,60 @@ class Viewer(models.Model):
     def __str__(self):
         return f'user_name = {self.user_name}'
 
-class Films_Watched(models.Model):
+class Films_Watched(models.Model): #Has Many Through
     appointment_date = models.DateTimeField(default=timezone.localtime())
     film_id = models.ForeignKey(Film, on_delete=models.CASCADE, related_name='films')
     viewer_id = models.ForeignKey(Viewer, on_delete=models.CASCADE, related_name='viewers')
     
     def __str__(self):
-        return f'film_id = {self.film_id}, viewer_id = {self.viewer_id}'
+        return f'film {self.film_id}, viewer {self.viewer_id}'
 
 
 
+# 3. Imagine an app that allows users to keep track of the books they've read:
+class Author(models.Model): #Author must be written first.
+    name = models.CharField(max_length=255)
+    biography = models.TextField(max_length=255)
 
+    def __str__(self):
+        return f'name = {self.name}'
 
+class Book(models.Model): #Book must be written first.
+    title = models.CharField(max_length=255)
+    year = models.IntegerField()
+
+    def __str__(self):
+        return f'title = {self.title}'
+
+class Reader(models.Model): #Reader must be written first.
+    user_name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    age = models.IntegerField()
+    favourite_genre = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'user_name = {self.user_name}'
+
+class Authorship(models.Model): #Has Many Through
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='authors')
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='book_authors')
+
+    def __str__(self):
+        return f'author {self.author}, book {self.book}'
+
+class Books_Read(models.Model): #Has Many Through
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='books')
+    reader = models.ForeignKey(Reader, on_delete=models.CASCADE, related_name='readers')
+
+    def __str__(self):
+        return f'book {self.book}, reader {self.reader}'
+
+class Chapter(models.Model):
+    title = models.CharField(max_length=255)
+    length = models.IntegerField()
+    summary = models.CharField(max_length=255)
+    book_id = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='book_chapters')
+
+    def __str__(self):
+        return f'title = {self.title}'
